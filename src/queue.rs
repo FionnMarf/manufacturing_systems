fc592a141cb6de677e3303440537816adeeb3fa9
fionno <fionnoconaill@gmail.com>
2023-09-08 17:45:31 +0100

    reqriting the queue stuff, still haven't tested anything though

--- src/queue.rs
+++ src/queue.rs
@@ -1,12 +1,71 @@
 // a struct representing an M/M/1 queue
 // $lambda: the arrival rate
 // $mu: the service rate
-pub fn mm1(lambda: f64, mu: f64) -> MarkovChain {
-    let mut states = Vec::new();
-    states.push(State::new("0".to_string()));
-    states.push(State::new("1".to_string()));
-    let mut transitions = Vec::new();
-    transitions.push(Transition::new(0, 1, lambda));
-    transitions.push(Transition::new(1, 0, mu));
-    MarkovChain::new(states, transitions)
+// $size: the number of items in the queue
+pub struct Queue {
+    lambda: f64,
+    mu: f64,
+    size: usize,
+}
+
+impl Queue {
+    pub fn new(lambda: f64, mu: f64, size: usize) -> Queue {
+        Queue {
+            lambda: lambda,
+            mu: mu,
+            size: size,
+        }
+    }
+
+    // returns the average number of items in the queue
+    pub fn avg_num_items(&self) -> f64 {
+        self.lambda / (self.mu - self.lambda)
+    }
+
+    // returns the average time an item spends in the queue
+    pub fn avg_time_in_queue(&self) -> f64 {
+        1.0 / (self.mu - self.lambda)
+    }
+
+    // returns the probability that the queue is empty
+    pub fn prob_empty(&self) -> f64 {
+        1.0 - (self.lambda / self.mu)
+    }
+
+    // returns the probability that the queue is full
+    pub fn prob_full(&self) -> f64 {
+        (self.lambda / self.mu).powi(self.size as i32)
+    }
+
+    // returns the probability that the queue is at a given level
+    pub fn prob_level(&self, level: usize) -> f64 {
+        (self.lambda / self.mu).powi(level as i32)
+    }
+
+    // returns the probability that the queue is at a given level or higher
+    pub fn prob_level_or_higher(&self, level: usize) -> f64 {
+        let mut prob = 0.0;
+        for i in level..self.size + 1 {
+            prob += self.prob_level(i);
+        }
+        prob
+    }
+
+    // returns the probability that the queue is at a given level or lower
+    pub fn prob_level_or_lower(&self, level: usize) -> f64 {
+        let mut prob = 0.0;
+        for i in 0..level + 1 {
+            prob += self.prob_level(i);
+        }
+        prob
+    }
+
+    // returns the probability that the queue is at a given level or between two levels
+    pub fn prob_level_between(&self, level1: usize, level2: usize) -> f64 {
+        let mut prob = 0.0;
+        for i in level1..level2 + 1 {
+            prob += self.prob_level(i);
+        }
+        prob
+    }
 }
