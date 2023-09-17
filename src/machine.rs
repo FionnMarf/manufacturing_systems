03db0f94b57614a0886cbd7434510332d195a419
fionno <fionnoconaill@gmail.com>
2023-09-17 12:41:31 +0100

    refactoring machine to allow multiple machines to share a buffer

--- src/machine.rs
+++ src/machine.rs
@@ -1,14 +1,15 @@
 use crate::queue::Buffer;
 use crate::markov::MarkovChain;
 use crate::create_machine_chain;
+use std::sync::{Arc, Mutex};
 
 pub struct Machine {
     pub markov_chain: MarkovChain,
     pub processing_time: f64,
     pub num_items: usize,
     pub output_name: Option<String>,
-    pub input_buffer: Option<Buffer>,
-    pub output_buffer: Option<Buffer>,
+    pub input_buffer: Option<Arc<Mutex<Buffer>>>,
+    pub output_buffer: Option<Arc<Mutex<Buffer>>>,
 }
 
 impl Machine {
@@ -36,12 +37,22 @@ impl Machine {
         }
     }
 
-    pub fn add_input_buffer(&mut self, capacity: usize, throughput: Option<f64>) {
-        self.input_buffer = Some(Buffer::new(capacity, throughput, None));
+    pub fn create_and_add_input_buffer(&mut self, capacity: usize, throughput: Option<f64>) {
+        let buffer = Buffer::new(capacity, throughput, None);
+        self.input_buffer = Some(Arc::new(Mutex::new(buffer)));
     }
 
-    pub fn add_output_buffer(&mut self, capacity: usize, throughput: Option<f64>) {
-        self.output_buffer = Some(Buffer::new(capacity, throughput, self.output_name.clone()));
+    pub fn create_and_add_output_buffer(&mut self, capacity: usize, throughput: Option<f64>) {
+        let buffer = Buffer::new(capacity, throughput, self.output_name.clone());
+        self.output_buffer = Some(Arc::new(Mutex::new(buffer)));
+    }
+
+    pub fn set_input_buffer(&mut self, buffer: Arc<Mutex<Buffer>>) {
+        self.input_buffer = Some(buffer);
+    }
+
+    pub fn set_output_buffer(&mut self, buffer: Arc<Mutex<Buffer>>) {
+        self.output_buffer = Some(buffer);
     }
 
     pub fn add_item(&mut self) {
@@ -57,71 +68,101 @@ impl Machine {
     }
 
     pub fn num_items_in_input_buffer(&self) -> usize {
-        match self.input_buffer {
-            Some(ref buffer) => buffer.num_items(),
+        match &self.input_buffer {
+            Some(buffer) => {
+                let locked_buffer = buffer.lock().unwrap();
+                locked_buffer.num_items
+            },
             None => 0,
         }
     }
 
     pub fn num_items_in_output_buffer(&self) -> usize {
-        match self.output_buffer {
-            Some(ref buffer) => buffer.num_items(),
+        match &self.output_buffer {
+            Some(buffer) => {
+                let locked_buffer = buffer.lock().unwrap();
+                locked_buffer.num_items(),
+            },
             None => 0,
         }
     }
 
     pub fn is_input_buffer_full(&self) -> bool {
-        match self.input_buffer {
-            Some(ref buffer) => buffer.is_full(),
+        match &self.input_buffer {
+            Some(buffer) => {
+                let locked_buffer = buffer.lock().unwrap();
+                locked_buffer.is_full(),
+            },
             None => false,
         }
     }
 
     pub fn is_output_buffer_full(&self) -> bool {
-        match self.output_buffer {
-            Some(ref buffer) => buffer.is_full(),
+        match &self.output_buffer {
+            Some(buffer) => {
+                let locked_buffer = buffer.lock().unwrap();
+                locked_buffer.is_full(),
+            },
             None => false,
         }
     }
 
     pub fn is_input_buffer_empty(&self) -> bool {
-        match self.input_buffer {
-            Some(ref buffer) => buffer.is_empty(),
+        match &self.input_buffer {
+            Some(buffer) => {
+                let locked_buffer = buffer.lock().unwrap();
+                locked_buffer.is_empty(),
+            },
             None => false,
         }
     }
 
     pub fn is_output_buffer_empty(&self) -> bool {
-        match self.output_buffer {
-            Some(ref buffer) => buffer.is_empty(),
+        match &self.output_buffer {
+            Some(buffer) => {
+                let locked_buffer = buffer.lock().unwrap();
+                locked_buffer.is_empty(),
+            },
             None => false,
         }
     }
 
     pub fn add_item_to_input_buffer(&mut self) {
-        match self.input_buffer {
-            Some(ref mut buffer) => buffer.add_item(),
+        match &mut self.input_buffer {
+            Some(mut buffer) => {
+                let mut locked_buffer = buffer.lock().unwrap();
+                locked_buffer.add_item(),
+            },
             None => (),
         }
     }
 
     pub fn add_item_to_output_buffer(&mut self) {
-        match self.output_buffer {
-            Some(ref mut buffer) => buffer.add_item(),
+        match &mut self.output_buffer {
+            Some(mut buffer) => {
+                let mut locked_buffer = buffer.lock().unwrap();
+                locked_buffer.add_item(),
+            },
             None => (),
         }
     }
 
     pub fn remove_item_from_input_buffer(&mut self) {
-        match self.input_buffer {
-            Some(ref mut buffer) => buffer.remove_item(),
+        match &mut self.input_buffer {
+            Some(mut buffer) => {
+                let mut locked_buffer = buffer.lock().unwrap();
+                locked_buffer.remove_item(),
+            },
             None => (),
         }
     }
 
     pub fn remove_item_from_output_buffer(&mut self) {
-        match self.output_buffer {
-            Some(ref mut buffer) => buffer.remove_item(),
+        match &mut self.output_buffer {
+            Some(ref mut buffer) => {
+                let mut locked_buffer = buffer.lock().unwrap();
+                locked_buffer.remove_item(),
+            },
             None => (),
         }
     }
@@ -130,13 +171,13 @@ impl Machine {
         // step the markov chain forward
         MarkovChain::step_chain(&mut self.markov_chain);
         // step the input buffer forward
-        match self.input_buffer {
-            Some(ref mut buffer) => self.remove_item_from_input_buffer(),
+        match &mut self.input_buffer {
+            Some(mut buffer) => self.remove_item_from_input_buffer(),
             None => (),
         }
         // step the output buffer forward
-        match self.output_buffer {
-            Some(ref mut buffer) => self.add_item_to_output_buffer(),
+        match &mut self.output_buffer {
+            Some(mut buffer) => self.add_item_to_output_buffer(),
             None => (),
         }
     }
@@ -154,4 +195,14 @@ pub struct Item {
     pub name: String,
     pub size: f64,
     pub cost: Option<f64>,
+}
+
+impl Item {
+    pub fn new(name: String, size: f64, cost: Option<f64>) -> Item {
+        Item {
+            name: name,
+            size: size,
+            cost: cost,
+        }
+    }
 }
