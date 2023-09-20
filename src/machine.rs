cf3815acdb88960aa5564761e36aa328566f23e2
fionno <fionnoconaill@gmail.com>
2023-09-20 18:48:04 +0100

    beginning big refactor into tokio

--- src/machine.rs
+++ src/machine.rs
@@ -1,7 +1,8 @@
 use crate::queue::Buffer;
 use crate::markov::MarkovChain;
 use crate::create_machine_chain;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use uuid::Uuid;
 
 pub struct Machine {
@@ -130,6 +131,131 @@ impl Machine {
 
     pub fn get_output_name(&self) -> Option<String> {
         self.output_name.clone()
+    }
+}
+
+// creating the async aware version outside the impl block
+
+
+
+
+
+pub struct Buffer {
+    pub id: Uuid,
+    pub name: Option<String>,
+    pub capacity: usize,
+    pub num_items: usize,
+    pub throughput: Option<f64>,
+    pub items: Vec<(Arc<Item>, f64)>,
+}
+
+impl Buffer {
+    pub fn new(capacity: usize, throughput: Option<f64>, name: Option<String>) -> Buffer {
+        Buffer {
+            id: Uuid::new_v4(),
+            name: name,
+            capacity: capacity,
+            num_items: 0,
+            throughput: throughput,
+            items: Vec::new(),
+        }
+    }
+
+    pub fn add_item(&mut self) {
+        if self.num_items < self.capacity {
+            self.num_items += 1;
+        }
+    }
+
+    pub fn remove_item(&mut self) {
+        if self.num_items > 0 {
+            self.num_items -= 1;
+        }
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.num_items == self.capacity
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.num_items == 0
+    }
+
+    pub fn num_items(&self) -> usize {
+        self.num_items
+    }
+
+    pub fn set_throughput(&mut self, throughput: f64) {
+        self.throughput = Some(throughput);
+    }
+
+    pub fn get_throughput(&self) -> Option<f64> {
+        self.throughput
+    }
+
+    pub fn change_name(&mut self, name: String) {
+        self.name = Some(name);
+    }
+}
+
+enum BufferEvent {
+    ChangeItem { item: Item, quantity: f64 },
+    CreateBuffer { capacity: usize, throughput: Option<f64>, name: Option<String> },
+}
+
+pub async fn change_items_to_buffer_by_id(
+    &self,
+    tx: &tokio::sync::mpsc::Sender<(Uuid, BufferEvent)>,
+    buffer_id: Uuid,
+    item: Item,
+    quantity: f64,
+) -> Result<(), &'static str> {
+    // Send a change request event.
+    tx.send((buffer_id, BufferEvent::ChangeItem { item, quantity })).await.map_err(|_| "Failed to send change request")
+}
+
+pub async fn create_buffer(
+    &self,
+    tx: &tokio::sync::mpsc::Sender<(Uuid, BufferEvent)>,
+    capacity: usize,
+    throughput: Option<f64>,
+    name: Option<String>,
+) {
+    // Send a create buffer event.
+    tx.send((Uuid::new_v4(), BufferEvent::CreateBuffer { capacity, throughput, name })).await.map_err(|_| "Failed to send create buffer request")
+}
+
+pub async fn buffer_event_handler(rx: tokio::sync::mpsc::Receiver<(Uuid, BufferEvent)>, buffers: Vec<tokio::sync::Mutex<Buffer>>) {
+    while let Some((buffer_id, event)) = rx.recv().await {
+        match event {
+            BufferEvent::ChangeItem { item, quantity } => {
+                if let Some(buffer) = buffers.iter().find(|b| b.lock().await.id == buffer_id) {
+                    let mut locked_buffer = buffer.lock().await;
+
+                    // Try to find the item in the buffer's items list.
+                    if let Some((_item_ref, item_quantity)) = locked_buffer.items.iter_mut().find(|(existing_item, _)| existing_item.id == item.id) {
+                        if *item_quantity + quantity < 0.0 {
+                            // Handle the error, possibly with logging or other mechanisms.
+                        } else {
+                            *item_quantity += quantity;
+                        }
+                    } else {
+                        if quantity >= 0.0 {
+                            locked_buffer.items.push((Arc::new(item), quantity));
+                        } else {
+                            // Handle the error.
+                        }
+                    }
+                } else {
+                    // Handle the buffer not found error.
+                }
+            }
+            BufferEvent::CreateBuffer { capacity, throughput, name } => {
+                // Create a new buffer and add it to the buffers vector.
+                let mut buffer = Buffer::new(capacity, throughput, name);
+                buffers.push(tokio::sync::Mutex::new(buffer));
+            }
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,27 @@ use std::collections::HashSet;
 use crate::markov::{MarkovChain};
 use crate::transfer_lines::TransferLine;
 use uuid::Uuid;
+use tokio::time::{delay_for, Duration};
+use tokio::sync::mpsc::{channel, Sender, Receiver};
 
 mod markov;
 mod transfer_lines;
 mod queue;
 mod machine;
-// test again
-fn main() {
-    // create a markov chain for testing
-    let mut machine_1 = markov::MarkovChain {
-        states: Vec::new(),
-        transitions: Vec::new(),
-    };
-    // add some states
-    let state_0 = machine_1.add_state("Idle".to_string());
-    let state_1 = machine_1.add_state("Working".to_string());
-    let state_2 = machine_1.add_state("Broken".to_string());
-    // add some transitions
-    machine_1.add_transition(state_0, state_1, 0.5);
-    machine_1.add_transition(state_0, state_2, 0.5);
-    machine_1.add_transition(state_1, state_2, 0.5);
-    machine_1.add_transition(state_1, state_0, 0.5);
-    machine_1.add_transition(state_2, state_0, 0.5);
-    machine_1.add_transition(state_2, state_1, 0.5);
 
-    let state_0 = machine_1.get_state_name(0);
-    println!("State 0: {}", state_0);
-
-    MarkovChain::step_chain(&mut machine_1);
-    let current_state = machine_1.get_current_state_name();
-    println!("Current state: {}", current_state);
-
-    let processing_times = vec![3.0, 2.0, 1.0];
-    let capacities = vec![3, 3];
-    let throughputs = vec![None, None];
-    let transfer_line = TransferLine::new(processing_times, capacities, throughputs);
-
-    let mut copper_wire_belt = queue::Buffer::new(30, None, Some("copper_wire".to_string()));
-    let mut copper_plate_belt = queue::Buffer::new(30, None, Some("copper_plate".to_string()));    
-    let mut iron_plate_belt = queue::Buffer::new(30, None, Some("iron_plate".to_string()));
-
+#[tokio::main]
+async fn main() {
+    // we are writing a tokio based async aware main function for our simulation
+    // we will use the tokio::time::delay_for() function to simulate the passage of time
+    // we will use the tokio::sync::mpsc::channel() function to create a channel for sending messages
+    let (tx, rx) = mpsc::channel(32);
+    let mut transfer_line = TransferLine::new(vec![1.0, 1.0], vec![1, 1], vec![None, None]);
+    transfer_line.add_machine(1.0, None);
+    transfer_line.add_buffer(1, None);
+    let ticker = tokio::spawn(async move {
+        loop {
+            sleep(Duration::from_millis(1)).await; // Represents a very small time increment
+            // Emit time events or update global state
+        }
+    });
 }
-// test commit time
